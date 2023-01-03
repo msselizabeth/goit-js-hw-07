bb384bbcdf6dcafe8036b3a30eb6e9248ef5aa84
msselizabeth <lizagotovitryby@gmail.com>
2023-01-03 19:27:34 +0200

    1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+function createGalleryItems (galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+}).join('');
+}
+
+function onGalleryItemClick(event){
+    event.preventDefault();
+
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+      }
+
+      const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show();
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+function createGalleryItems (galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`
+}).join('');
+}
+
+function onGalleryItemClick (evt){
+    evt.preventDefault();
+
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+      }
+
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+     });
+}
